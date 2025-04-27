03a4acd6cea5e819aed9f377dc5a2ba6480fa0f9
Sam El-Anis <sam.elanis@microsoft.com>
2025-04-27 22:34:37 +0100

    Initial commit.

---
+++ AppServiceDotNet9/ContosoPortal/Pages/MemberArea.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPortal.Pages
+{
+    [Authorize(Roles = "Member")]
+    public class MemberAreaModel : PageModel
+    {
+        private readonly ILogger<MemberAreaModel> _logger;
+
+        public MemberAreaModel(ILogger<MemberAreaModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ AppServiceDotNetFramework4.8.1/ContosoPortal/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Claims = ClaimsPrincipal.Current.Claims;
+            return View();
+        }
+        // ENV variables WEBSITE_AUTH_USE_LEGACY_CLAIMS needs to set as false in App Service
+        // before App Roles will work due to the format of the "roles" claims
+        // https://learn.microsoft.com/en-us/azure/app-service/reference-app-settings?tabs=kudu%2Cdotnet
+        [Authorize(Roles = "Member")]
+        public ActionResult MemberArea()
+        {
+            ViewBag.Message = "Only users with the app role Member can see this page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
